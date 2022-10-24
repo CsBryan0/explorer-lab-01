8e86b64aff7eaade840a65f64cf3d2f6b700d2ef
unknown <souzabryan09@gmail.com>
2022-10-24 19:16:15 -0300

    finalizado2

--- src/main.js
+++ src/main.js
@@ -4,22 +4,32 @@ import IMask from "imask"
 //seleciona a cor do cartão
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
-
-//seleciona a imagem do cartão
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+const background = document.querySelector(".cc")
 
 function setCardType(type) {
   const colors = {
-    visa: ["#436D99", "#2D57F2"],
-    mastercard: ["#C69347", "#DF6F29"],
-    elo: ["#FFD94A", "#068CD8"],
-    default: ["black", "gray"],
+    visa: {
+      background: "url('./visa.svg')",
+    },
+    mastercard: {
+      background: "url('./mastercard.svg')",
+    },
+    elo: {
+      background: "url('./elo.svg')",
+    },
+    default: {
+      background: "url('./cc-default.svg')",
+    },
   }
 
-  ccBgColor01.setAttribute("fill", colors[type][0])
-  ccBgColor02.setAttribute("fill", colors[type][1])
-  ccLogo.setAttribute("src", `cc-${type}.svg`)
+  background.style.backgroundImage = colors[type].background
+  ccLogo.setAttribute("src", `./cc-${type}.svg`)
 }
+
+/*
+
+*/
 
 globalThis.setCardType = setCardType
 
@@ -65,12 +75,16 @@ const cardNumberPattern = {
       regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardtype: "mastercard",
     },
-    /*gi  {
+    {
       mask: "0000 0000 0000 0000",
-      regex:
-        /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8])) \d{0,10}/,
-      cardType: "elo",
-    },*/
+      regex: /^6\d{0,15}/,
+      cardtype: "elo",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^606282|^3841(?:[0|4|6]{1})0/,
+      cardtype: "hipercard",
+    },
     {
       mask: "0000 0000 0000 0000",
       cardtype: "default",
